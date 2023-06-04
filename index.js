dab801306faaf02d7916714830e336f3319169c8
ArefShojaei <arefshojaei82dev@gmail.com>
2023-06-04 17:59:21 +0330

    communicating with DOM and getting HTML form the url

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
 
 const run = async () => {
     const browser = await puppeteer.launch({
@@ -10,7 +11,29 @@ const run = async () => {
 
     const page = await browser.newPage()
     
-    await page.goto("https://google.com")
+    await page.goto("https://www.digikala.com/search/category-mobile-phone/apple/")
+
+    await page.waitForTimeout(10000) // wait for "10 seconds"
+
+    // communicating with DOM
+    await page.evaluate(() => {
+        return new Promise((resolve, reject) => {
+            const scrollInterval = setInterval(() => {
+                const scroll_height = document.body.scrollHeight
+                const scroll_y = window.scrollY
+
+                scroll_y += 800;
+                window.scroll(0, scroll_y);
+    
+                if (scroll_y > scroll_height) {
+                    clearInterval(scrollInterval);
+                    resolve()
+                }
+            }, 4000);
+        })
+    })
+
+    const html = await page.content()
 
     await browser.close()
 }
