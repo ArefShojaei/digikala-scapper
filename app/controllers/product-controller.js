f6692f7dd46a06930cc769433d01c6a224953eec
ArefShojaei <arefshojaei82dev@gmail.com>
2023-06-11 23:25:05 +0330

    add comments for more informaiton of the processes

--- app/controllers/product-controller.js
+++ app/controllers/product-controller.js
@@ -3,13 +3,17 @@ const fsPromise = require('fs/promises');
 
 module.exports = async (req, res) => {
     try {
+        // split url with '/' for handling params like */api/category/{ categoryName }/{ name }
         const [, category, name] = req.url.split("/")
 
+        // get JSON file content
         const response = await fsPromise.readFile(path.join(__dirname, `../data/${category}/${name}.json`), "utf-8")
         
+        // parse the JSON file content
         const products = JSON.parse(response)
     
-        return res.json(products)
+
+        return res.status(200).json(products)
     } catch (err) {
         return res.status(500).json({
             status : "Error",
