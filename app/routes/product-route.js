90eb8be364b53fee6d174895d2b62296ffda19be
ArefShojaei <arefshojaei82dev@gmail.com>
2023-06-11 23:26:26 +0330

    refactore code structure for handling processes

--- app/routes/product-route.js
+++ app/routes/product-route.js
@@ -1,20 +1,29 @@
 const { Router } = require("express");
 const router = Router()
 const path = require("path");
-const extractJSON = require('../utils/extractJSON-util');
+const extractJSON = require('@utils/extractCategoriesJSON-util');
 const productController = require('@controllers/product-controller');
 
 const initializeRoutes = async() => {
-    const jsonFileDetails = await extractJSON(path.join(__dirname, "../../categories.config.json"))
-    const { categoryName, names } = jsonFileDetails;
+    // get JSON config details
+    const extractedCategoriesJSON = await extractJSON(path.join(__dirname, "../../categories.config.json"))
 
-    // console.log(categoryName, names);
+    for(const currentCategory of extractedCategoriesJSON) {
+        const { categoryName, names } = currentCategory
 
-    for(const name of names) {
-        router.get(`/${categoryName}/${name}`, productController)
+        names.forEach(name => {
+            // initialize route by the config like categoryName and name 
+
+            /**
+             ** For Example : /api/category/{dynamic_categoryName}/{dynamic_name} 
+            */
+
+            router.get(`/${categoryName}/${name}`, productController)
+        })
     }
 }
 
+// run for initializing all routes of the config
 initializeRoutes()
 
 module.exports = router
